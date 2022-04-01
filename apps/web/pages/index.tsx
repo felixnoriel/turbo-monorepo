17b37727207a324db639ce5fd752cd4ba2db09b7
felixnoriel <jrnoriel_56@yahoo.com>
2022-04-01 14:00:25 +1100

    change colour to secondary

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button color="primary" variant="contained">
+      <Button color="secondary" variant="contained">
         Subscribe
       </Button>
     </div>
